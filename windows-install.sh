e596a0d4932d7a8a15a2794b6809304ba4a24663
Xsnipe2Z <yassinend2017@gmail.com>
2025-06-05 00:42:33 +0100

    Update installation script: modify ISO download link and adjust partitioning commands

--- windows-install.sh
+++ windows-install.sh
@@ -2,6 +2,32 @@
 
 set -e  # Exit on any error
 
+apt install -y linux-image-amd64
+
+apt install -y grub2 wimtools ntfs-3g rsync wget gdisk parted
+
+# Get disk size and calculate partition sizes
+disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print int($3)}')
+disk_size_mb=$((disk_size_gb * 1024))
+part_size_mb=$((disk_size_mb / 4))
+
+
+parted /dev/sda --script mklabel gpt
+parted /dev/sda --script mkpart primary ntfs 1MB ${part_size_mb}MB
+parted /dev/sda --script mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
+
+sleep 5
+
+partprobe /dev/sda
+
+sleep 5
+
+mkfs.ntfs -f /dev/sda1
+mkfs.ntfs -f /dev/sda2
+
+
+echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
+
 
 mount /dev/sda1 /mnt
 
@@ -25,11 +51,11 @@ EOF
 
 # Download Windows ISO
 cd /root/windisk
-wget -O win2019.iso "http://bit.ly/4mPJOQE"
+wget -O win2022.iso "https://bit.ly/winwin22022"
 
 # Mount and extract ISO
 mkdir winfile
-mount -o loop win2019.iso winfile
+mount -o loop win2022.iso winfile
 rsync -avh --progress winfile/ /mnt/
 umount winfile
 
